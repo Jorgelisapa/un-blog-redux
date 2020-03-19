3b0663d5757e01db4538ef8678504b756d53804e
Jorgelisapa <jorgelisapa@gmail.com>
2020-03-18 20:22:48 -0500

    Aplicando estilos básicos a tabla de contenido - v5

--- src/components/App.js
+++ src/components/App.js
@@ -2,47 +2,47 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
+    <div className='margen' >
       <div>un blog/react y redux - @Jorgelisapa</div>
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Nombre
-          </th>
-          <th>
-            Correo
-          </th>
-          <th>
-            Enlace
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            Jorge Salamanca
+      <table className='tabla' >
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Jorge Salamanca
+              </td>
+            <td>
+              jorgelisapa@gmail.com
+              </td>
+            <td>
+              jorgesalamanca.com
             </td>
-          <td>
-            jorgelisapa@gmail.com
+          </tr>
+          <tr>
+            <td>
+              emtibi
+              </td>
+            <td>
+              hola@emtibi.com
+              </td>
+            <td>
+              emtibi.com
             </td>
-          <td>
-            jorgesalamanca.com
-          </td>
-        </tr>
-        <tr>
-          <td>
-            emtibi
-            </td>
-          <td>
-            hola@emtibi.com
-            </td>
-          <td>
-            emtibi.com
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
