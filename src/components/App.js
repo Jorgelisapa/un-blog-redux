ffd4c84960634f37a046a074109346252ad9735b
Jorgelisapa <jorgelisapa@gmail.com>
2020-03-18 19:58:46 -0500

    Creando tabla de contenido

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,48 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>un blog/react y redux</div>
+    <div>
+      <div>un blog/react y redux - @Jorgelisapa</div>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Jorge Salamanca
+            </td>
+          <td>
+            jorgelisapa@gmail.com
+            </td>
+          <td>
+            jorgesalamanca.com
+          </td>
+        </tr>
+        <tr>
+          <td>
+            emtibi
+            </td>
+          <td>
+            hola@emtibi.com
+            </td>
+          <td>
+            emtibi.com
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
   );
 }
 
