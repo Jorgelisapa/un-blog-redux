e4a468817478a39ccfa4f0d1c18c05b51f69f91d
Jorgelisapa <jorgelisapa@gmail.com>
2020-03-24 21:29:45 -0500

    Llamado a url de API con axioS y uso de de async await para llamada  asincrona - v8

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -8,7 +9,9 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log('respuesta: ', respuesta);
     this.setState({
       usuarios: [
         {
